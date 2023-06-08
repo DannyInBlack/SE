618eb1dc609039b50b8427d61da50038955a7b07
DannyInBlack <danielghobrial11@gmail.com>
2023-06-08 08:42:09 +0300

    imagine actually doing something this dumb

--- src/firebase_setup/firebase.js
+++ src/firebase_setup/firebase.js
@@ -6,7 +6,7 @@ let db = false;
 export const getDb = () => {
     if(!db){
         const firebaseConfig = {
-          apiKey: import.meta.env.VITE_APP_ID ,
+          apiKey: import.meta.env.VITE_API_KEY ,
           authDomain: import.meta.env.VITE_AUTH_DOMAIN,
           projectId: import.meta.env.VITE_PROJECT_ID,
           storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
