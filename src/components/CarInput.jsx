673fb9f599dd18459eef3449b06889c887ddbe35
DannyInBlack <danielghobrial11@gmail.com>
2023-06-08 12:12:03 +0300

    more button styling and etc

--- src/components/CarInput.jsx
+++ src/components/CarInput.jsx
@@ -4,7 +4,7 @@ function CarInput(props){
     <div id='adding' style={{visibility:"hidden"}}>
       <input value={props.plate} onChange={e => props.setPlate(e.target.value)} className='inputs' type='text' placeholder="Plate"/>
       <input value={props.model} onChange={e => props.setModel(e.target.value)} className='inputs' type='text' placeholder="Model"/>
-      <button onClick={() => {props.onSubmitClick()}}>Submit</button>
+      <button id="submit-btn" onClick={() => {props.onSubmitClick()}}>Submit</button>
     </div>
   )
 }
